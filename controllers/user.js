3a31ea813671c9576c718903e9d4835c2d2860ee
adamalbari <anjaybanget1@gmail.com>
2023-11-07 15:57:33 +0700

    video 11 dan 12 selesai

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,4 @@
-let users = [
-    {id: 1, nama: 'Adam', email:'anjaybanget1@gmail.com'},
-    {id: 2, nama: 'kaine', email:'reikok31@gmail.com'},
-  ]
+const User = require('../models/User')
 
   module.exports = {
     index: (req, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const userrouter = require('./router/users')
-
+const connectDB = require('./config/db') 
 const port = 3000
 
 app.use(express.json()) // for parsing application/json
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {
 })
 
 app.use(userrouter)
+
+connectDB()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
